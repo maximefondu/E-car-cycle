a7f1c35f2497e8fe9a3c40067f2eabe98085a0b3
maximeFondu <maximefondu@icloud.com>
2020-03-25 22:08:42 +0100

    map + data interactive

--- src/scripts/components/map.js
+++ src/scripts/components/map.js
@@ -1,6 +1,8 @@
-const map = document.querySelector('.map')
-const circles = map.children
-const numberUse = []
+const mapSvg         = document.querySelector('.map__svg')
+const numberPosition = document.querySelector('.map__number')
+const typePosition   = document.querySelector('.map__type')
+const circles        = mapSvg.childNodes
+const numberUse      = []
 
 //GetData
 fetch(`./public/json/number-car.json`)
@@ -8,17 +10,46 @@ fetch(`./public/json/number-car.json`)
 .then(data => {
 
     data.forEach(element => {
-        const pourcent = element['pourcent']
-        const slug     = element['slug']
-        const pointNumber = Math.round(pourcent / 100 * circles.length)
+        const pourcent     = element['pourcent']
+        const slug         = element['slug']
+        const numberCircle = Math.round(pourcent / 100 * circles.length)
 
-        for (let i = 0; i < pointNumber; i++) {
-            const circle = getRandomInt(circles.length)    
-            circles[circle].setAttribute('data-type', slug)
+        if(slug !== 'all'){
+            for (let i = 0; i < numberCircle; i++) {
+                const circle = getRandomInt(circles.length)
+                circles[circle].setAttribute('data-type', slug)
+            }    
+        }else{
+            const number = getNumber(element['number'])
+            const plural = element['plural']
+            numberPosition.textContent=number
+            typePosition.textContent=`voitures aux ${plural}`
         }
-    });
+
+    })
+
+    circles.forEach(circle => { 
+        circle.addEventListener('mouseover', (e)=>{
+            displayData(data, e)
+        })
+
+        circle.addEventListener('mouseout', (e)=>{
+            const defaultData = data.find(data => data.id == 7)
+            const number = getNumber(defaultData['number'])
+            const plural = defaultData['plural']
+    
+            //TEXT
+            numberPosition.textContent = number
+            typePosition.textContent = `voitures ${plural}`
+
+            //COLORS
+            colorsRemove()
+        })
+    })
 
 })
+
+
 
 //RANDOM INT
 function getRandomInt(max){    
@@ -29,8 +60,50 @@ function getRandomInt(max){
         return number
     }else{
         return getRandomInt(max)
-        console.log('ELSE');
-        
     }
 }
 
+//NUMBER 3 SPACES
+function getNumber(element){
+    const number = new Intl.NumberFormat().format(element)
+    return number
+}
+
+//DISPLAY COLORS
+function colors(slug){
+    circles.forEach(circle => { 
+        circle.classList.add('disable')
+
+        const currentSlug = circle.getAttribute('data-type')
+        if(currentSlug === slug){
+            circle.classList.remove('disable')
+        }
+    })
+}
+
+//REMOVE COLORS
+function colorsRemove(){
+    circles.forEach(circle => {
+        circle.classList.remove('disable')
+    })
+}
+
+//DISPLAY
+function displayData(data, e){
+    const element        = e.currentTarget
+    const slug           = element.getAttribute('data-type')
+    const currentData    = data.find(data => data.slug == slug)
+    const plural         = currentData["plural"]
+    const number         = getNumber(currentData['number'])
+    const currentNumber  = numberPosition.textContent
+    
+    //SI SURVOLE UNE DATA DIFFERENTE
+    if(currentNumber !== number){
+        //TEXT
+        numberPosition.textContent = number
+        typePosition.textContent = `voitures ${plural}`
+
+        //COLORS
+        colors(slug)
+    } 
+}
--- templates/sections/number-car.php
+++ templates/sections/number-car.php
@@ -1,6 +1,13 @@
-<section class='wrapper wrapper--padding'>
-    <h2 class='title'>Combien de voiture électrique/hybride en Belgique</h2>
+<section class='wrapper wrapper--padding map'>
+    <h2 class='title grid__1-8'>Combien de voiture électrique/hybride en Belgique</h2>
 
-    <?= print_svg('map-belgium', 'map') ?>
+    <div class='map__content'>
+        <?= print_svg('map-belgium', 'map__svg') ?>
+        <div class='map__data'>
+            <span class='map__number'></span>
+            <h3 class='map__type'></h3>
+        </div>
+    </div>
+
 
 </section>
